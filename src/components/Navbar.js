90c6ac982b79094543848243f6a0c323251fa24a
ihor.nechypor <ihor.nechypor@plej.pl>
2020-08-01 12:58:51 +0200

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => (
     <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
@@ -8,14 +8,14 @@ export const Navbar = () => (
         </div>
         <ul className="navbar-nav">
             <li className="nav-item">
-                <a href="/" className="nav-link">
+                <NavLink exact to="/" className="nav-link">
                     Main
-                </a>
+                </NavLink>
             </li>
             <li className="nav-item">
-                <a href="/about" className="nav-link">
+                <NavLink to="/about" className="nav-link">
                     Information
-                </a>
+                </NavLink>
             </li>
         </ul>
     </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export const About = () => {
 
-    return(
-        <div>
-            <h1>
-                About page
-            </h1>
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">Information</h1>
+            <p className="lead">
+                Some information
+            </p>
         </div>
     )
 }
