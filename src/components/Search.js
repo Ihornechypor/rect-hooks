3aefcd22454bd2652218f72c2c5aa725cb9f9d34
ihor.nechypor <ihor.nechypor@plej.pl>
2020-08-01 14:41:33 +0200

    lesson 118

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {AlertContext} from "../context/AlertContext";
 
 export const Search = () => {
+    const [value, setValue] = useState('')
     const {show} = useContext(AlertContext);
 
     const onSubmit = event => {
-        if(event.key === 'Enter'){
-            show('this is alert')
+        if(event.key !== 'Enter'){
+            return
+        }
+
+        if(value.trim()){
+            console.log('request')
+        } else {
+            show('Enter user data')
         }
 
     }
@@ -17,6 +24,8 @@ export const Search = () => {
                 type="text"
                 className="form-control"
                 placeholder="enter nick name"
+                value={value}
+                onChange={event => setValue(event.target.value)}
                 onKeyPress={onSubmit}
             />
 
